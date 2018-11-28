bac740a526fb71f320c0e7892f982b39217fb892
savke5 <44963923+savke5@users.noreply.github.com>
2018-11-28 11:22:08 +0100

    Add files via upload

---
+++ DiteSpanZadatak/DiteSpanZadatak/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiteSpanZadatak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime novaGodina = new DateTime(DateTime.Now.Year + 1, 1, 1);
+            TimeSpan preostalo = novaGodina - DateTime.Now;
+            Console.WriteLine("Do Nove godine preostalo je:");
+            Console.WriteLine("{0} dana,{1} sati,{2} minuta ,{3}sekundi",preostalo.Days,preostalo.Hours,preostalo.Minutes,preostalo.Seconds);
+
+     
+
+
+           Console.WriteLine("U danima: {0}", preostalo.TotalDays);
+           Console.WriteLine("U satima: {0}", preostalo.TotalHours);
+           Console.WriteLine("U min.: {0}", preostalo.TotalMinutes);
+           Console.WriteLine("U sek.: {0}", preostalo.TotalSeconds);
+           Console.ReadKey();
+        }
+    }
+}
