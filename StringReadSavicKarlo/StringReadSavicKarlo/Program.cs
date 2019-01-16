35e0f958cb08c941f7b2e5b77b8a883dde727b26
savke5 <44963923+savke5@users.noreply.github.com>
2019-01-16 17:32:32 +0100

    Add files via upload

---
+++ StringReadSavicKarlo/StringReadSavicKarlo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace StringReadSavicKarlo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(@"D:\My Documents\NOOP\datoteka.txt");
+          
+
+            while (!sr.EndOfStream)
+            {
+                Console.WriteLine(sr.ReadLine());
+            }
+            
+
+            sr.Close();
+            Console.ReadKey();
+        }
+    }
+}
