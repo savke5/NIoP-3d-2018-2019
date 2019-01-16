8d2d5bf25adfd4beb0f1e1a5398847c034a06fdd
savke5 <44963923+savke5@users.noreply.github.com>
2019-01-16 17:46:01 +0100

    Add files via upload

---
+++ SavicKarloUsing/SavicKarloUsing/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SavicKarloUsing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesite ime: ");
+            string ime = Console.ReadLine();
+            Console.Write("Unesite prezime: ");
+            string prezime = Console.ReadLine();
+            // Podatke upisujemo u datoteku
+            Console.WriteLine("\n-- Zapisujemo u datoteku...");
+            using (StreamWriter sw = new StreamWriter(@"C:\Users\Učenik\source\repos\SavicKarloUsing\datoteka.txt"))
+            {
+                sw.WriteLine("Ime: {0}", ime);
+                sw.WriteLine("Prezime: {0}", prezime);
+            }
+
+  
+
+// Podatke čitamo iz datoteke
+            Console.WriteLine("\n-- Pročitano iz datoteke:");
+            using (StreamReader sr = new StreamReader(@"C:\Users\Učenik\source\repos\SavicKarloUsing\datoteka.txt"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    Console.WriteLine(sr.ReadLine());
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+    }
+
