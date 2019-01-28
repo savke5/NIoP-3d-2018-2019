58a4d542722994f17822b62f7d23afe3cf3e0f87
savke5 <44963923+savke5@users.noreply.github.com>
2019-01-28 10:18:20 +0100

    Add files via upload

---
+++ SavicKarloMonthCalendarKontrola/SavicKarloMonthCalendarKontrola/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SavicKarloMonthCalendarKontrola
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TimeSpan ts = monthCalendar1.SelectionEnd - monthCalendar1.SelectionStart;
+            if (ts.Days < 1)
+            {
+                MessageBox.Show("Korištenjem tipke SHIFT odaberite opseg datuma.");
+                return;
+            }
+            MessageBox.Show("Rezervirali ste usluge u trajanju od " + ts.Days.ToString() + "dana.", "Rezervacija");
+            MessageBox.Show("Vasa rezervacija počinje " + monthCalendar1.SelectionStart.ToShortDateString() + " a završava se " + monthCalendar1.SelectionEnd.ToShortDateString());
+
+        }
+
+        private void oProgramuToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form2 licenca = new Form2();
+            licenca.Show();
+        }
+    }
+}
