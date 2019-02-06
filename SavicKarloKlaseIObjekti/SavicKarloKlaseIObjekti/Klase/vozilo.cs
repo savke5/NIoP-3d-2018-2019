849d5f4ec7fd3de0451bb4e9d372bd20437a76c7
savke5 <44963923+savke5@users.noreply.github.com>
2019-02-06 12:09:59 +0100

    Add files via upload

---
+++ SavicKarloKlaseIObjekti/SavicKarloKlaseIObjekti/Klase/vozilo.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SavicKarloKlaseIObjekti.Klase
+{
+    class Vozilo
+    {
+        int brojKotaca;
+        bool leti, pliva, vozi;
+        string vrsta;
+        string oznaka;
+
+        public void setBrojKotaca(int brojKotaca)
+        {
+            this.brojKotaca = brojKotaca;
+        }
+
+        public int getbrojKotaca()
+        {
+            return this.brojKotaca;
+
+        }
+
+        public void setLeti(bool leti)
+        {
+            this.leti = leti;
+
+        }
+
+        public bool getLeti()
+        {
+            return this.leti;
+        }
+
+        public void setPliva(bool pliva)
+        {
+            this.pliva = pliva;
+
+        }
+
+        public bool getPliva()
+        {
+            return this.pliva;
+        }
+
+        public void setVozi(bool vozi)
+        {
+            this.vozi = vozi;
+
+        }
+
+        public bool getVozi()
+        {
+            return this.vozi;
+        }
+
+
+        public void setOznaka(string oznaka)
+        {
+            this.oznaka = oznaka;
+        
+        }
+
+        public string getOznaka()
+        {
+            return this.oznaka;
+
+        }
+
+        public void setVrsta(string vrsta)
+        {
+            this.vrsta = vrsta;
+
+        }
+
+        public string getVrsta()
+        {
+            return this.vrsta;
+
+        }
+
+        public Vozilo(int brojKotaca, bool leti, bool pliva, bool vozi, string vrsta, string oznaka)
+        {
+            this.brojKotaca = brojKotaca;
+            this.leti = leti;
+            this.pliva = pliva;
+            this.vozi = vozi;
+            this.vrsta = vrsta;
+            this.oznaka = oznaka;
+        }
+
+        public Vozilo()
+        {
+
+        }
+        public override string ToString()
+        {
+            return "\n Vrsta:" + vrsta + "\n Oznaka: " + oznaka + "\n Pliva: " + pliva + "\n Leti" + leti
+                + "\n Vozi: " + vozi + "\n Broj kotaca: " + brojKotaca;
+        }
+
+    }
+}
---
+++ SavicKarloKlaseIObjekti/SavicKarloKlaseIObjekti/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SavicKarloKlaseIObjekti.Klase;
+
+namespace SavicKarloKlaseIObjekti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vozilo avijon = new Vozilo();
+            avijon.setBrojKotaca(6);
+            avijon.setLeti(true);
+            avijon.setPliva(false);
+            avijon.setVozi(true);
+            avijon.setVrsta("Cessna");
+            avijon.setOznaka("1312");
+            Console.WriteLine("\n Vrsta:" + avijon.getVrsta() + "\n Oznaka: " + avijon.getOznaka() + "\n Leti:"
+
+                + avijon.getLeti() + "\n Pliva" + avijon.getPliva() + "\n Vozi: " + avijon.getVozi() + "\n Broj kotaca: " + avijon.getbrojKotaca());
+
+            Vozilo kamion = new Vozilo(8, false, false, true, "TAM", "RI1987AR");
+
+            Console.WriteLine(kamion.ToString());
+
+
+
+            Console.ReadLine();
+
+
+           
+
+        }
+    }
+}
