53789b218d120b0c3fe21c5ec961625266a46ea5
savke5 <44963923+savke5@users.noreply.github.com>
2019-02-12 14:51:30 +0100

    Add files via upload

---
+++ KlaseObjektiKonstruktori01-master/klaseAndObjekti/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using klaseAndObjekti.Klase;
+
+namespace klaseAndObjekti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vozilo ime = new Vozilo();
+
+
+            bool josupisa = true;
+            
+            do
+            {
+                Console.WriteLine("Kojoj vrsti pripada:");
+                ime.setVrsta(Console.ReadLine().ToString());
+                Console.WriteLine("Koliko kotača ima vaše vozilo(ako nema niti jedan napšite 0)");
+                ime.setBrojKotaca(Convert.ToInt32(Console.ReadLine()));
+                Console.WriteLine("Može li vaše vozilo letjeti(true/false):");
+                ime.setLeti(bool.Parse(Console.ReadLine()));
+                Console.WriteLine("Može li vaše vozilo plivati(true/false):");
+                ime.setPliva(bool.Parse(Console.ReadLine()));
+                Console.WriteLine("Može li vaše vozilo voziti(true/false):");
+                ime.setVozi(bool.Parse(Console.ReadLine()));
+                Console.WriteLine("Koja je oznaka vašeg vozila:");
+                ime.setOznaka(Console.ReadLine().ToString());
+
+                Console.WriteLine(ime.ispis());
+
+                Console.WriteLine("Želite li dodati još neko vozilo?(true/false)");
+                josupisa = bool.Parse(Console.ReadLine());
+            } while (josupisa == true);
+
+           
+
+
+            Console.ReadLine();
+        }
+    }
+
+    
+}
